298abac18a47d77800b549b0990d732a34d283cd
apokalipsa44 <m_lachowicz@wp.pl>
2019-10-10 23:34:48 +0200

    fetch working

--- src/components/App.js
+++ src/components/App.js
@@ -3,50 +3,73 @@ import Form from './Form'
 import Result from './Result'
 import './App.css';
 
+const API_KEY = '&APPID=a0a75a5ef8ff7f89910316e0784e7828';
 
 class App extends React.Component {
   state = {
-    value: '', 
+    value: '',
     city: '',
-    date:'',
-    sunrise:'',
-    sunset:'',
-    temperature:'',
-    wind:'',
-    pressure:'',
-    formError:''
+    date: '',
+    sunrise: '',
+    sunset: '',
+    temperature: '',
+    wind: '',
+    pressure: '',
+    formError: false
   }
 
   onWriteUnlock = (e) => {
     this.setState({ value: e.target.value })
   }
 
-  handleCitySubmit=(e)=>{
+  handleCitySubmit = (e) => {
     e.preventDefault()
     // const API = 'http://samples.openweathermap.org/data/2.5/weather?q=${this.state.value},uk&appid=a0a75a5ef8ff7f89910316e0784e7828?units=metric'
-    const API = 'http://api.openweathermap.org/data/2.5/weather?q='+this.state.value+'&APPID=a0a75a5ef8ff7f89910316e0784e7828'
+    const API = 'http://api.openweathermap.org/data/2.5/weather?q=' + this.state.value + API_KEY
     console.log("city submited")
     fetch(API)
-    .then(response => {
-      if(response.ok){
-        return response
-      }
-    })
-    .then(response=>response.json())
-    .then(gucio=>console.log(gucio))
-    .catch(formError=> console.log(formError))
+      .then(response => {
+        if (response.ok) {
+          return response
+        }
+      })
+      .then(response => response.json())
+      .then(gucio => {
+        const time = new Date().toLocaleString();
+        this.setState(prevState => ({
+          date: time,
+          sunrise: gucio.sys.sunrise,
+          sunset: gucio.sys.sunset,
+          temperature: gucio.main.temp,
+          wind: gucio.wind.speed,
+          pressure: gucio.main.pressure,
+          formError: false,
+          city: this.state.value
+        }))
+      })
+      .catch(formError => {
+        console.log(formError);
+        this.setState(stan => ({
+          formError: true,
+          city: this.state.value
+        }))
+      })
   }
+
 
   render() {
     return (
       <div className="App">
-        <Form value={this.state.value} unlock={this.onWriteUnlock} submit={this.handleCitySubmit}/>
-        <Result />
+        <Form value={this.state.value} unlock={this.onWriteUnlock} submit={this.handleCitySubmit} />
+        <Result weatherObject={this.state} />
 
       </div>
     );
+
   }
 
+
 }
+
 
 export default App;
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
-const Result = () => {
+const Result = props => {
+    const{value,
+    city,
+    date,
+    sunrise,
+    sunset,
+    temperature,
+    wind,
+    pressure,
+    formError}=props.weatherObject
     return(
         <div>
-            <button>baton</button>
+           <React.Fragment>
+               <div>{city}</div>
+               <div>{date}</div>
+               <div>{sunset}</div>
+               <div>{sunrise}</div>
+               <div>{temperature}</div>
+               <div>{wind}</div>
+               <div>{pressure}</div>
+               <div>{formError}</div>
+           </React.Fragment>
         </div>
     )
 }
